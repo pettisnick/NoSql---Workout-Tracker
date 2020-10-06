25f5eac52c8ae4b0f7b17068ad847deaeed0d60f
Nick Pettis <pettisnick@gmail.com>
2020-10-06 14:19:08 -0400

    adding files

---
+++ models/workout.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+    day: {
+        type: Date,
+        default: Date.now
+    },
+    exercises: [{
+        type: {
+            type: String,
+            enum: ['resistance', 'cardio'],
+            required: true
+        },
+        name: {
+            type: String,
+            required: true
+        },
+        weight: {
+            type: Number,
+            required: function () {
+                return this.type === "resistance"
+            }
+        },
+        sets: {
+            type: Number,
+            required: function () {
+                return this.type === "resistance"
+            }
+        },
+        reps: {
+            type: Number,
+            required: function () {
+                return this.type === "resistance"
+            }
+        },
+        duration: {
+            type: String,
+            required: true
+        },
+        distance: {
+            type: Number,
+            required: function () {
+                return this.type === "cardio"
+            }
+        }
+    }
+    ]
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
